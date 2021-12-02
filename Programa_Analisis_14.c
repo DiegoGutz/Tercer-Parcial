912419807cd8bf75a44f6ef5643af42e3aa4b7ca
DiegoGutz <88503186+DiegoGutz@users.noreply.github.com>
2021-12-02 12:52:52 -0600

    Update Programa_Analisis_14.c

--- Programa_Analisis_14.c
+++ Programa_Analisis_14.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 main()
 {
-    float a, b, c, x;
-    printf("Calculo de las ecuacion de primer grado ax+b=c");
-    printf("introduzca los valores de a, b y c: ");
-    scanf("%f%f%f", &a, &b, &c);
-    x= (c-b)/a;
-    printf("El resultado de esta ecuación para los valores%1.f, %1.f y %1.f es: %2f", a, b, c, x);
-    return 0;
+int a, b, c, x;
+printf("introduzca los valores de a, b y c: ");
+scanf("%d%d%d", &a, &b, &c);
+printf("Los valores iniciales son: \n a=%d\n b=%d\n c=%d\n", a, b, c);
+x=a;
+a=b;
+b=c;
+c=x;
+printf("Los valores finales son:\n a=%d\n b=%d\n c=%d\n", a, b, c);
 }
